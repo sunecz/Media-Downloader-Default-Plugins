a2975f42c865e9144f4ec3f5f6a0d4dc8e3ca8fd
Sune <sune@suneweb.net>
2022-07-10 04:41:03 +0200

    HTML Server: Released version 0003.

--- server.html5/sune/app/mediadown/server/html5/HTML5ServerPlugin.java
+++ server.html5/sune/app/mediadown/server/html5/HTML5ServerPlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.server.Servers;
 
 @Plugin(name          = "server.html5",
 	    title         = "plugin.server.html5.title",
-	    version       = "0002",
+	    version       = "0003",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/server/html5/",
 	    updatable     = true,
