331b19cb17f6a238db94fd09bc72006e18f53e41
Sune <sune@suneweb.net>
2022-07-28 13:05:54 +0200

    iPrima ME: Released version 0017.

--- media_engine.iprima/sune/app/mediadown/media_engine/iprima/IPrimaEnginePlugin.java
+++ media_engine.iprima/sune/app/mediadown/media_engine/iprima/IPrimaEnginePlugin.java
@@ -11,7 +11,7 @@ import sune.app.mediadown.util.Password;
 
 @Plugin(name          = "media_engine.iprima",
 	    title         = "plugin.media_engine.iprima.title",
-	    version       = "0016",
+	    version       = "0017",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/media_engine/iprima/",
 	    updatable     = true,
