247bf33c11ab2678bbed0d7833be77883b06cc42
Sune <sune@suneweb.net>
2022-09-22 20:56:08 +0200

    YouTube Server: Released version 0010.

--- server.youtube/sune/app/mediadown/server/youtube/YouTubeServerPlugin.java
+++ server.youtube/sune/app/mediadown/server/youtube/YouTubeServerPlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.server.Servers;
 
 @Plugin(name          = "server.youtube",
 	    title         = "plugin.server.youtube.title",
-	    version       = "0009",
+	    version       = "0010",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/server/youtube/",
 	    updatable     = true,
