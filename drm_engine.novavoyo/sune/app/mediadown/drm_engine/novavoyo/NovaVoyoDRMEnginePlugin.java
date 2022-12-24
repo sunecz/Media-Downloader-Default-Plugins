a4c9f20b48a3fc704ec16566a56ad957c2f0efb6
Sune <sune@suneweb.net>
2022-12-24 18:48:09 +0100

    Nova Voyo DE: Released version 00.02.08-0002.

--- drm_engine.novavoyo/sune/app/mediadown/drm_engine/novavoyo/NovaVoyoDRMEnginePlugin.java
+++ drm_engine.novavoyo/sune/app/mediadown/drm_engine/novavoyo/NovaVoyoDRMEnginePlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadownloader.drm.DRMEngines;
 
 @Plugin(name          = "drm_engine.novavoyo",
 	    title         = "plugin.drm_engine.novavoyo.title",
-	    version       = "00.02.08-0001",
+	    version       = "00.02.08-0002",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/drm_engine/novavoyo/",
 	    updatable     = true,
