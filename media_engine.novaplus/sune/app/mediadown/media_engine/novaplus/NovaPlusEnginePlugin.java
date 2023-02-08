cf1bb26145d5a57fd50d79cbcb73b14235f28ebc
Sune <sune@suneweb.net>
2023-02-08 01:32:18 +0100

    TV Nova ME: Released version 00.02.08-0004.

--- media_engine.novaplus/sune/app/mediadown/media_engine/novaplus/NovaPlusEnginePlugin.java
+++ media_engine.novaplus/sune/app/mediadown/media_engine/novaplus/NovaPlusEnginePlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.plugin.PluginBase;
 
 @Plugin(name          = "media_engine.novaplus",
 	    title         = "plugin.media_engine.novaplus.title",
-	    version       = "00.02.08-0003",
+	    version       = "00.02.08-0004",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/media_engine/novaplus/",
 	    updatable     = true,
