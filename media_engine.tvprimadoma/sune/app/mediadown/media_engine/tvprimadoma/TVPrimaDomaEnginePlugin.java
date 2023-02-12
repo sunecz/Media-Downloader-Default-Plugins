995de3b926e376d55dda7b75ae77de5caa3ffd81
Sune <sune@suneweb.net>
2023-02-12 16:17:40 +0100

    TV Prima Doma ME: Released version 00.02.08-0004.

--- media_engine.tvprimadoma/sune/app/mediadown/media_engine/tvprimadoma/TVPrimaDomaEnginePlugin.java
+++ media_engine.tvprimadoma/sune/app/mediadown/media_engine/tvprimadoma/TVPrimaDomaEnginePlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.plugin.PluginBase;
 
 @Plugin(name          = "media_engine.tvprimadoma",
 	    title         = "plugin.media_engine.tvprimadoma.title",
-	    version       = "00.02.08-0003",
+	    version       = "00.02.08-0004",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/media_engine/tvprimadoma/",
 	    updatable     = true,
