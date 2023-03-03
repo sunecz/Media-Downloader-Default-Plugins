1202dda77d9a923282f2e353fabd4b3e95468fbb
Sune <sune@suneweb.net>
2023-03-03 01:12:12 +0100

    media_engine.streamcz: Released version 00.02.08-0005.

--- media_engine.streamcz/sune/app/mediadown/media_engine/streamcz/StreamCZEnginePlugin.java
+++ media_engine.streamcz/sune/app/mediadown/media_engine/streamcz/StreamCZEnginePlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.plugin.PluginBase;
 
 @Plugin(name          = "media_engine.streamcz",
 	    title         = "plugin.media_engine.streamcz.title",
-	    version       = "00.02.08-0004",
+	    version       = "00.02.08-0005",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/media_engine/streamcz/",
 	    updatable     = true,
