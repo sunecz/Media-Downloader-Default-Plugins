b19da7f94688f478801819c65d2b786ccbbc0cbf
Sune <sune@suneweb.net>
2023-06-30 01:54:08 +0200

    media_engine.ceskatelevize: Released version 00.02.09-0001.

--- media_engine.ceskatelevize/sune/app/mediadown/media_engine/ceskatelevize/CeskaTelevizeEnginePlugin.java
+++ media_engine.ceskatelevize/sune/app/mediadown/media_engine/ceskatelevize/CeskaTelevizeEnginePlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.plugin.PluginBase;
 
 @Plugin(name          = "media_engine.ceskatelevize",
 	    title         = "plugin.media_engine.ceskatelevize.title",
-	    version       = "00.02.08-0006",
+	    version       = "00.02.09-0001",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/media_engine/ceskatelevize/",
 	    updatable     = true,
