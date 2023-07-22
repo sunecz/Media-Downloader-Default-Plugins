57a524076cdfd9d45e3342911a858f05b07d6c56
Sune <sune@suneweb.net>
2023-07-23 00:47:48 +0200

    drm_engine.ceskatelevize: Updated to support the DRM plugin Version 2.

--- drm_engine.ceskatelevize/sune/app/mediadown/drm_engine/ceskatelevize/CeskaTelevizeDRMEngine.java
+++ drm_engine.ceskatelevize/sune/app/mediadown/drm_engine/ceskatelevize/CeskaTelevizeDRMEngine.java
@@ -1,28 +1,12 @@
 package sune.app.mediadown.drm_engine.ceskatelevize;
 
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.nio.file.Path;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
+import java.net.URI;
 
-import org.cef.browser.CefFrame;
-import org.cef.network.CefCookie;
-import org.cef.network.CefCookieManager;
-
-import io.netty.handler.codec.http.FullHttpRequest;
+import sune.app.mediadown.drm.DRMEngine;
+import sune.app.mediadown.drm.DRMResolver;
 import sune.app.mediadown.media.Media;
 import sune.app.mediadown.net.Net;
-import sune.app.mediadown.util.JSON;
-import sune.app.mediadown.util.JSON.JSONCollection;
-import sune.app.mediadownloader.drm.DRMBrowser;
-import sune.app.mediadownloader.drm.DRMContext;
-import sune.app.mediadownloader.drm.DRMEngine;
-import sune.app.mediadownloader.drm.DRMResolver;
-import sune.app.mediadownloader.drm.resolver.SimpleDRMResolver;
-import sune.app.mediadownloader.drm.util.JS;
-import sune.app.mediadownloader.drm.util.MPDQualityModifier;
+import sune.app.mediadown.net.Web.Request;
 
 public class CeskaTelevizeDRMEngine implements DRMEngine {
 	
@@ -31,20 +15,19 @@ public class CeskaTelevizeDRMEngine implements DRMEngine {
 	}
 	
 	@Override
-	public DRMResolver createResolver(DRMContext context, String url, Path output, Media media) {
-		return new CTDRMResolver(context, url, output, media);
+	public DRMResolver createResolver() {
+		return new CTDRMResolver();
 	}
 	
 	@Override
-	public boolean isCompatibleURL(String url) {
-		URL urlObj = Net.url(url);
+	public boolean isCompatibleURI(URI uri) {
 		// Check the protocol
-		String protocol = urlObj.getProtocol();
+		String protocol = uri.getScheme();
 		if(!protocol.equals("http") &&
 		   !protocol.equals("https"))
 			return false;
 		// Check the host
-		String[] hostParts = urlObj.getHost().split("\\.", 2);
+		String[] hostParts = uri.getHost().split("\\.", 2);
 		if(hostParts.length < 2
 				// Check only the second and top level domain names,
 				// since there are many subdomains, and there may be
@@ -55,84 +38,21 @@ public class CeskaTelevizeDRMEngine implements DRMEngine {
 		return true;
 	}
 	
-	private static final class CTDRMResolver extends SimpleDRMResolver {
+	private static final class CTDRMResolver implements DRMResolver {
 		
-		private static final String URL_IFRAME;
+		private static final URI LICENSE_URI;
 		
 		static {
-			URL_IFRAME = "https://player.ceskatelevize.cz/";
-		}
-		
-		public CTDRMResolver(DRMContext context, String url, Path output, Media media) {
-			super(context, url, output, media);
+			String accessToken = "c3RlcGFuLWEtb25kcmEtanNvdS1wcm9zdGUtbmVqbGVwc2k=";
+			LICENSE_URI = Net.uri("https://ivys-wvproxy.o2tv.cz/license?access_token=" + accessToken);
 		}
 		
 		@Override
-		public void onLoadStart(DRMBrowser browser, CefFrame frame) {
-			if(frame.getURL().startsWith(url)) {
-				// Set consent cookie to not show the cookie consent popup.
-				Instant instant = Instant.now();
-				String currentTimestamp = instant.toString();
-				Date now = Date.from(instant);
-				Date expires = Date.from(instant.plus(24, ChronoUnit.HOURS));
-				CefCookie cookieConsent = new CefCookie("OptanonAlertBoxClosed", currentTimestamp,
-					".ceskatelevize.cz", "/", false, false, now, now, false, expires);
-				CefCookieManager.getGlobalManager().setCookie(url, cookieConsent);
-			} else if(frame.getURL().startsWith(URL_IFRAME)) {
-				JS.Record.include(frame);
-				JS.Record.activate(frame, "video[class^='video-']");
-			}
-		}
-		
-		@Override
-		public void onLoadEnd(DRMBrowser browser, CefFrame frame, int httpStatusCode) {
-			if(frame.getURL().startsWith(url)) {
-				JS.Helper.include(frame);
-				JS.Helper.enableInterframeCommunication(frame);
-				JS.Helper.click(browser, frame, "main button");
-			} else if(frame.getURL().startsWith(URL_IFRAME)) {
-				JS.Helper.include(frame);
-				JS.Helper.hideVideoElementStyle(frame);
-			}
-		}
-		
-		@Override
-		public boolean shouldModifyResponse(String uri, String mimeType, Charset charset, FullHttpRequest request) {
-			return uri.contains("ivysilani/client-playlist/") || mimeType.equalsIgnoreCase("application/dash+xml");
-		}
-		
-		@Override
-		public String modifyResponse(String uri, String mimeType, Charset charset, String content,
-				FullHttpRequest request) {
-			if(uri.contains("ivysilani/client-playlist/")) {
-				JSONCollection json = JSON.read(content);
-				
-				// Remove ads from the playlist JSON data, so the recording is not interrupted
-				json.set("setup.vast.preRoll", JSONCollection.emptyArray());
-				json.set("setup.vast.midRoll", JSONCollection.emptyArray());
-				json.set("setup.vast.postRoll", JSONCollection.emptyArray());
-				
-				// Keep only VODs (the actual videos) in the playlist
-				JSONCollection newPlaylist = JSONCollection.emptyArray();
-				for(JSONCollection item : json.getCollection("playlist").collectionsIterable()) {
-					String type = item.getString("type");
-					if(type.equals("VOD")) newPlaylist.add(item);
-				}
-				json.set("playlist", newPlaylist);
-				
-				content = json.toString(true);
-			} else if(mimeType.equalsIgnoreCase("application/dash+xml")) {
-				// Select the quality we want
-				MPDQualityModifier modifier = MPDQualityModifier.fromString(content);
-				modifier.modify(media.quality());
-				content = modifier.xml().html();
-			}
-			return content;
-		}
-		
-		@Override
-		public String url() {
-			return url;
+		public Request createRequest(Media media) {
+			// The body will be replaced with content of the challenge
+			return Request.of(LICENSE_URI)
+				.addHeaders("Referer", "https://player.ceskatelevize.cz/")
+				.POST("", "application/octet-stream");
 		}
 	}
 }
