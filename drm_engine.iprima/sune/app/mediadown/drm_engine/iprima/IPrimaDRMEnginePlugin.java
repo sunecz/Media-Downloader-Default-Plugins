a6a0f8b340135cc6e729b4633cc9ad7f48f5e9a9
Sune <sune@suneweb.net>
2023-07-23 01:41:40 +0200

    drm_engine.iprima: Released version 00.02.09-0002.

--- drm_engine.iprima/sune/app/mediadown/drm_engine/iprima/IPrimaDRMEnginePlugin.java
+++ drm_engine.iprima/sune/app/mediadown/drm_engine/iprima/IPrimaDRMEnginePlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.plugin.PluginBase;
 
 @Plugin(name          = "drm_engine.iprima",
 	    title         = "plugin.drm_engine.iprima.title",
-	    version       = "00.02.09-0001",
+	    version       = "00.02.09-0002",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/drm_engine/iprima/",
 	    updatable     = true,
