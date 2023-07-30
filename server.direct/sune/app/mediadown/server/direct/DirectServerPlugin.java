2f331784d1ba30ee1bc894f3ac952ff08b82013d
Sune <sune@suneweb.net>
2023-07-30 11:05:43 +0200

    server.direct: Released version 00.02.09-0001.

--- server.direct/sune/app/mediadown/server/direct/DirectServerPlugin.java
+++ server.direct/sune/app/mediadown/server/direct/DirectServerPlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.plugin.PluginBase;
 
 @Plugin(name          = "server.direct",
 	    title         = "plugin.server.direct.title",
-	    version       = "00.02.09-0000",
+	    version       = "00.02.09-0001",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/server/direct/",
 	    updatable     = true,
