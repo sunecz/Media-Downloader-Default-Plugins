d377a22ed21ed4a609f14f08c27208261c9f0320
Sune <sune@suneweb.net>
2023-08-05 14:22:06 +0200

    media_engine.jojplay: Released version 00.02.09-0002.

--- media_engine.jojplay/sune/app/mediadown/media_engine/jojplay/JOJPlayEnginePlugin.java
+++ media_engine.jojplay/sune/app/mediadown/media_engine/jojplay/JOJPlayEnginePlugin.java
@@ -10,7 +10,7 @@ import sune.app.mediadown.util.Password;
 
 @Plugin(name          = "media_engine.jojplay",
 	    title         = "plugin.media_engine.jojplay.title",
-	    version       = "00.02.09-0001",
+	    version       = "00.02.09-0002",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/media_engine/jojplay/",
 	    updatable     = true,
