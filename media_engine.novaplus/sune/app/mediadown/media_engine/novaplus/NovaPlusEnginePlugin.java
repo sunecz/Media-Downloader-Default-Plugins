9d8844e96e6f9240c4f0f67d927977a1298f8014
Sune <sune@suneweb.net>
2023-08-08 15:14:59 +0200

    media_engine.novaplus: Released version 00.02.09-0006.

--- media_engine.novaplus/sune/app/mediadown/media_engine/novaplus/NovaPlusEnginePlugin.java
+++ media_engine.novaplus/sune/app/mediadown/media_engine/novaplus/NovaPlusEnginePlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.plugin.PluginBase;
 
 @Plugin(name          = "media_engine.novaplus",
 	    title         = "plugin.media_engine.novaplus.title",
-	    version       = "00.02.09-0005",
+	    version       = "00.02.09-0006",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/media_engine/novaplus/",
 	    updatable     = true,
