8aba4c01e18c9913dfa2baf2d06fe0c3924e3a12
Sune <sune@suneweb.net>
2023-08-13 23:46:23 +0200

    downloader.smf: Released version 00.02.09-0004.

--- downloader.smf/sune/app/mediadown/downloader/smf/SMFDownloaderPlugin.java
+++ downloader.smf/sune/app/mediadown/downloader/smf/SMFDownloaderPlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.plugin.PluginBase;
 
 @Plugin(name          = "downloader.smf",
 	    title         = "plugin.downloader.smf.title",
-	    version       = "00.02.09-0003",
+	    version       = "00.02.09-0004",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/downloader/smf/",
 	    updatable     = true)
