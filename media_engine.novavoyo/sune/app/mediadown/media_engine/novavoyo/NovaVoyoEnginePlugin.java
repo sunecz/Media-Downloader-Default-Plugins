c5c80c85791791142689aedbdd2e1229875c22eb
Sune <sune@suneweb.net>
2023-12-23 04:13:52 +0100

    media_engine.novavoyo: Released version 00.02.09-0012.

--- media_engine.novavoyo/sune/app/mediadown/media_engine/novavoyo/NovaVoyoEnginePlugin.java
+++ media_engine.novavoyo/sune/app/mediadown/media_engine/novavoyo/NovaVoyoEnginePlugin.java
@@ -17,7 +17,7 @@ import sune.app.mediadown.util.Password;
 
 @Plugin(name          = "media_engine.novavoyo",
 	    title         = "plugin.media_engine.novavoyo.title",
-	    version       = "00.02.09-0011",
+	    version       = "00.02.09-0012",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/media_engine/novavoyo/",
 	    updatable     = true,
