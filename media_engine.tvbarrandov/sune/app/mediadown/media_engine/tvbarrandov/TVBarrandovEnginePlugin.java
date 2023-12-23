a63da8853adb06393b8759ba0d2fa34badbdcf49
Sune <sune@suneweb.net>
2023-12-23 04:13:11 +0100

    media_engine.tvbarrandov: Released version 00.02.09-0005.

--- media_engine.tvbarrandov/sune/app/mediadown/media_engine/tvbarrandov/TVBarrandovEnginePlugin.java
+++ media_engine.tvbarrandov/sune/app/mediadown/media_engine/tvbarrandov/TVBarrandovEnginePlugin.java
@@ -17,7 +17,7 @@ import sune.app.mediadown.util.Password;
 
 @Plugin(name          = "media_engine.tvbarrandov",
 	    title         = "plugin.media_engine.tvbarrandov.title",
-	    version       = "00.02.09-0004",
+	    version       = "00.02.09-0005",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/media_engine/tvbarrandov/",
 	    updatable     = true,
