5fc0959763f890bcf3a4a51072fbe366a0b432ae
Sune <sune@suneweb.net>
2023-12-23 04:13:24 +0100

    media_engine.tvautosalon: Released version 00.02.09-0002.

--- media_engine.tvautosalon/sune/app/mediadown/media_engine/tvautosalon/TVAutosalonEnginePlugin.java
+++ media_engine.tvautosalon/sune/app/mediadown/media_engine/tvautosalon/TVAutosalonEnginePlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.plugin.PluginBase;
 
 @Plugin(name          = "media_engine.tvautosalon",
 	    title         = "plugin.media_engine.tvautosalon.title",
-	    version       = "00.02.09-0001",
+	    version       = "00.02.09-0002",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/media_engine/tvautosalon/",
 	    updatable     = true,
