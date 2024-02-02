9553815330ba83bc92095b2fd206ba94143a0630
Sune <sune@suneweb.net>
2024-02-02 08:20:49 +0100

    media_engine.markizavoyo: Fixed issue with not being able to obtain media sources after the saved device was deleted.

--- media_engine.markizavoyo/sune/app/mediadown/media_engine/markizavoyo/MarkizaVoyoEngine.java
+++ media_engine.markizavoyo/sune/app/mediadown/media_engine/markizavoyo/MarkizaVoyoEngine.java
@@ -481,7 +481,8 @@ public final class MarkizaVoyoEngine implements MediaEngine {
 					}
 					
 					switch(error.type()) {
-						case "player_not_logged_in": {
+						case "player_not_logged_in":
+						case "player_logged_in_no_access": {
 							// Only try a new token when this is the first attempt
 							if(attempt++ == 0) {
 								VoyoAccount.resetAuthCookie();
