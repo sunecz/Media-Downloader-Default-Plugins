702baef4ea46365ead131709381170e37d104165
Sune <sune@suneweb.net>
2024-03-05 02:36:22 +0100

    server.rtvs: Released version 00.02.09-0002.

--- server.rtvs/sune/app/mediadown/server/rtvs/RTVSServerPlugin.java
+++ server.rtvs/sune/app/mediadown/server/rtvs/RTVSServerPlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.plugin.PluginBase;
 
 @Plugin(name          = "server.rtvs",
 	    title         = "plugin.server.rtvs.title",
-	    version       = "00.02.09-0001",
+	    version       = "00.02.09-0002",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/server/rtvs/",
 	    updatable     = true,
