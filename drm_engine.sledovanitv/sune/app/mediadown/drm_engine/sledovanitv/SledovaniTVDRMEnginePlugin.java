f3726ba4eaf0b30175972d88a2e4399ba6b63b9c
Sune <sune@suneweb.net>
2024-04-08 09:10:43 +0200

    drm_engine.sledovanitv: Released version 00.02.09-0002.

--- drm_engine.sledovanitv/sune/app/mediadown/drm_engine/sledovanitv/SledovaniTVDRMEnginePlugin.java
+++ drm_engine.sledovanitv/sune/app/mediadown/drm_engine/sledovanitv/SledovaniTVDRMEnginePlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.plugin.PluginBase;
 
 @Plugin(name          = "drm_engine.sledovanitv",
 	    title         = "plugin.drm_engine.sledovanitv.title",
-	    version       = "00.02.09-0001",
+	    version       = "00.02.09-0002",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/drm_engine/sledovanitv/",
 	    updatable     = true,
