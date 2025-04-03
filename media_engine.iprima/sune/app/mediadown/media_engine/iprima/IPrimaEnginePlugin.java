8d1612c7b3effd1181bc0a823edc2b785b39c374
Sune <sune@suneweb.net>
2025-04-04 00:08:38 +0200

    media_engine.iprima: Released version 00.02.09-0023.

--- media_engine.iprima/sune/app/mediadown/media_engine/iprima/IPrimaEnginePlugin.java
+++ media_engine.iprima/sune/app/mediadown/media_engine/iprima/IPrimaEnginePlugin.java
@@ -35,7 +35,7 @@ import sune.app.mediadown.util.Password;
 
 @Plugin(name          = "media_engine.iprima",
 	    title         = "plugin.media_engine.iprima.title",
-	    version       = "00.02.09-0022",
+	    version       = "00.02.09-0023",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/media_engine/iprima/",
 	    updatable     = true,
