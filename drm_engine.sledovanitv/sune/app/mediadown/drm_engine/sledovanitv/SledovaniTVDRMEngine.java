c1040084cb3995ec8527d039d118e8a339e0b1f5
Sune <sune@suneweb.net>
2025-08-11 07:40:24 +0200

    drm_engine.sledovanitv: Update the request arguments to make it work.

--- drm_engine.sledovanitv/sune/app/mediadown/drm_engine/sledovanitv/SledovaniTVDRMEngine.java
+++ drm_engine.sledovanitv/sune/app/mediadown/drm_engine/sledovanitv/SledovaniTVDRMEngine.java
@@ -1,6 +1,7 @@
 package sune.app.mediadown.drm_engine.sledovanitv;
 
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 
 import sune.app.mediadown.drm.DRMEngine;
 import sune.app.mediadown.drm.DRMResolver;
@@ -42,13 +43,38 @@ public class SledovaniTVDRMEngine implements DRMEngine {
 	private static final class SledovaniTVDRMResolver implements DRMResolver {
 		
 		private static final String LICENSE_URI_TEMPLATE;
+		private static final long[] OBF = new long[] {
+			0x000000000019660dL, 0x000000003c6ef35fL,
+			0x5c06bd1601abc49eL, 0xe27b269cb54276cfL,
+			0x09b2a1bb63733ed6L, 0x405d34000000002bL,
+		};
 		
 		static {
 			LICENSE_URI_TEMPLATE = "https://drm.srv.czcloud.i.mtvreg.com/license/prod/widevine"
 					+ "?login=%{profile_id}s"
-					+ "&password=ukyg3uh16g1394gp6wdd"
+					+ "&password=" + de(OBF)
 					+ "&device=%{device_id}s"
 					+ "&streamURL=%{stream_url}s";
+		}
+		
+		private static final String de(long[] a) {
+			final long p = a[0], q = a[1], y = 0xffL, z = ~y | y, t = z >>> 32L;
+			final int c = (int) (a[a.length - 1] & t); final byte[] b = new byte[c];
+			for(int d = 0; d < c; b[d] = (byte) ((a[d >> 3] >> (0x38 - (((d++) & 0x7) << 3))) & 0xff));
+			final int m = 0xff, u = b[16] & m, v = b[17] & m, w = b[18] & m;
+			final int o = ~(1 << 5) & 0x37, n = b.length - o;
+			long e = ((((((b[19] & y) << 8) | (b[20] & y)) << 8) | (b[21] & y)) << 8) | (b[22] & y), s;
+			for(int i = 1, k, j, f; i < n; ++i) {
+				for(s = (e ^ n) & z, k = n - 1; k >= i; --k, s = (s * p + q) & t);
+				j = (int) (((s >>> 1) % (i + 1))); f = -((((i ^ j) | -(i ^ j)) >>> 31));
+				f = (b[o+i] ^ b[o+j]) & f; b[o+i] ^= f; b[o+j] ^= f;
+			}
+			for(int i = 0, x; i < n; ++i) {
+				x = ((b[o+i] & m) - ((i ^ w) & m)) & m;
+				x = ((x >>> ((i + v) & 7)) | (x << (8 - ((i + v) & 7)))) & m;
+				b[o+i] = (byte) (x ^ (u + (i * 31)) & m);
+			}
+			return new String(b, o, n, StandardCharsets.UTF_8);
 		}
 		
 		@Override
