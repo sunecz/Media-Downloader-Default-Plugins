ebf2092503670ac9ac4d96f0d716ccc9de089386
Sune <sune@suneweb.net>
2025-12-28 19:19:45 +0100

    server.sledovanitv: Released version 00.02.09-0011.

--- server.sledovanitv/sune/app/mediadown/server/sledovanitv/SledovaniTVServerPlugin.java
+++ server.sledovanitv/sune/app/mediadown/server/sledovanitv/SledovaniTVServerPlugin.java
@@ -21,7 +21,7 @@ import sune.app.mediadown.util.Password;
 
 @Plugin(name          = "server.sledovanitv",
 	    title         = "plugin.server.sledovanitv.title",
-	    version       = "00.02.09-0010",
+	    version       = "00.02.09-0011",
 	    author        = "Sune",
 	    updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/server/sledovanitv/",
 	    updatable     = true,
